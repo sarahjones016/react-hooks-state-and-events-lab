ce9ae0153bce807d8042502bf2b02055d1843971
Sarah Jones <sarahrjones8@gmail.com>
2023-04-11 12:57:38 -0400

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [isInCart, setIsInCart] = useState(false);
+  
+  const liClass = isInCart ? "in-cart" : ""
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart"
+  const buttonColor = isInCart ? "remove" : "add"
+
+  function cartClickHandler() {
+    setIsInCart(!isInCart);
+  }
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonColor} onClick={cartClickHandler}>{buttonText}</button>
     </li>
   );
 }
 
 export default Item;
+ 
